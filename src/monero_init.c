bf45f2e6f5b87a51e1761d6d4ac4dca113ddacfe
Sean Darcy <sean@darcyfinancial.com>
2020-06-25 15:30:19 +1000

    Update for Loki Client Version 7.X

--- src/monero_init.c
+++ src/monero_init.c
@@ -173,7 +173,7 @@ void monero_install(unsigned char netId) {
 /* ----------------------------------------------------------------------- */
 #define MONERO_SUPPORTED_CLIENT_SIZE 1
 const char * const monero_supported_client[MONERO_SUPPORTED_CLIENT_SIZE] = {
-  "6.",
+  "7.",
 };
 
 int monero_apdu_reset() {
